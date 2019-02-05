018447616f5411676d2991cc8754ffc6133ba976
Patrick Poulain <petitchevalroux@free.fr>
2019-02-05 22:11:30 +0100

    Add toJson and fromJson to map

--- packages/dataset-classes-map/tests/index.js
+++ packages/dataset-classes-map/tests/index.js
@@ -41,4 +41,20 @@ describe("ClassesMap", () => {
             assert.equal(map.getClass(2), "class3");
         });
     });
+
+    describe("toJSON", () => {
+        it("return json", () => {
+            assert.equal(map.toJSON(), "{\"classesToIntegers\":[[\"class1\",0],[\"class2\",1],[\"class3\",2],[\"class4\",3]],\"integersToClasses\":[[0,\"class1\"],[1,\"class2\"],[2,\"class3\"],[3,\"class4\"]],\"classes\":[\"class1\",\"class2\",\"class3\",\"class1\"]}");
+        });
+    });
+
+    describe("fromJSON", () => {
+        it("load from json string", () => {
+            const newMap = new ClassesMap();
+            newMap.fromJSON("{\"classesToIntegers\":[[\"class1\",0],[\"class2\",1],[\"class3\",2],[\"class4\",3]],\"integersToClasses\":[[0,\"class1\"],[1,\"class2\"],[2,\"class3\"],[3,\"class4\"]],\"classes\":[\"class1\",\"class2\",\"class3\",\"class1\"]}");
+            assert.deepEqual(newMap.toIntegers(), [0, 1, 2, 0]);
+            assert.equal(newMap.getClass(2), "class3");
+            assert.equal(newMap.get("class2"), 1);
+        });
+    });
 });
